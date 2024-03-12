67cd4ac98c5f07f6d3ba4c2ac218abd11a0d3068
Sarbjot-Gill <mrgill3377@gmail.com>
2024-03-12 10:35:06 +0530

    Mini Project

---
+++ we.js
@@ -0,0 +1,49 @@
+async function getWeather() {
+    var data
+    const loc = document.getElementById("ser").value;
+    console.log(loc)
+    var cond ;
+
+
+
+    const url = 'https://yahoo-weather5.p.rapidapi.com/weather?location=' + loc + '&format=json&u=c';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '57b246d315mshbd5e35002b0f4bap144ffcjsn22ddf69cd6b3',
+            'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        
+        data = JSON.parse(result);
+        cond = data.current_observation.condition.text;
+        
+    } catch (error) {
+        console.error(error);
+    }
+    console.log(data)
+    
+    document.getElementById("city").innerText = data.location.city
+    document.getElementById("temp").innerText = data.current_observation.condition.temperature + "°C"
+    document.getElementById("hum").innerText = data.current_observation.atmosphere.humidity + "%"
+    document.getElementById("wind").innerText = data.current_observation.wind.chill + "km/h"
+    if(cond === "Foggy"){
+        document.getElementById("condimg").src = "clouds.png"
+    }
+    else if(cond === "Fair"){
+        document.getElementById("condimg").src = "clear.png"
+    }
+    else if(cond === "Sunny"){
+        document.getElementById("condimg").src = "clear.png"
+    }
+    else if(cond === "Rain"){
+        document.getElementById("condimg").src = "rain.png"
+    }
+    else if(cond === "Snow"){
+        document.getElementById("condimg").src = "snow.png"
+    }
+}
